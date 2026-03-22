e74203887afd1ff62eacd211e6b2afcb42a6a5f5
Kern Rocke <44078438+kernrocke@users.noreply.github.com>
2026-03-22 08:44:28 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/sop-helper">
         <div className="flex flex-col min-h-screen">
           <SiteHeader />
           <div className="flex-1">
